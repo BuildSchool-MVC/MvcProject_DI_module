45ce1064f67656ddc85fc7f2b26172dee0e994c3
A Kuan <s11996123@gmail.com>
2018-05-17 12:08:19 +0800

    更新productservice

--- MvcProject/ModelsLibrary/Services/ProductsService.cs
+++ MvcProject/ModelsLibrary/Services/ProductsService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ModelsLibrary.DtO_Models;
+using ModelsLibrary.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,41 @@ namespace ModelsLibrary.Services
 {
     public class ProductsService
     {
+        ProductsRepository repository = new ProductsRepository();
+
+        public void Create(Products model)
+        {
+            repository.Create(model);
+        }
+
+        public void UpdateUnitPrice(Products model)
+        {
+            repository.UpdateUnitPrice(model);
+        }
+
+        public void UpdateStock(Products model,int input)
+        {
+            repository.UpdateStock(model,input);
+        }
+
+        public void UpdateDowntime(Products model)
+        {
+            repository.UpdateDowntime(model);
+        }
+
+        public Products FindByID(string productid)
+        {
+            return repository.FindByID(productid);
+        }
+
+        public IEnumerable<Products> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public IEnumerable<Products> GetColor(string Color)
+        {
+            return repository.GetColor(Color);
+        }
     }
 }
