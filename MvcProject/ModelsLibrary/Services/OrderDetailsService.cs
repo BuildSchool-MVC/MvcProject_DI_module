f22871bf84f8b0d13d9287fdab3d6e014620cd69
佩衿 蔡 <b10320030@gmail.com>
2018-05-17 12:00:31 +0800

    update OrderDetailsService

---
+++ MvcProject/ModelsLibrary/Services/OrderDetailsService.cs
@@ -0,0 +1,43 @@
+﻿using ModelsLibrary.DtO_Models;
+using ModelsLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLibrary.Services
+{
+    public class OrderDetailsService
+    {
+        public void Create(OrderDetails model)
+        {
+            var repository = new OrderDetailsRepository();
+            repository.Create(model);
+        }
+
+        public void Update(OrderDetails model)
+        {
+            var repository = new OrderDetailsRepository();
+            repository.Update(model);
+        }
+
+        public void Delete(OrderDetails model)
+        {
+            var repository = new OrderDetailsRepository();
+            repository.Delete(model);
+        }
+
+        public IEnumerable<OrderDetails> FindById(string OrderId)
+        {
+            var repository = new OrderDetailsRepository();
+            return repository.FindById(OrderId);
+        }
+
+        public IEnumerable<OrderDetails> GetAll()
+        {
+            var repository = new OrderDetailsRepository();
+            return repository.GetAll();
+        }
+    }
+}
