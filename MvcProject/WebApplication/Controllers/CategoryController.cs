bf8c0f310be8e0f192187c99b95a39919950e7b3
BingBing <z42442412@gmail.com>
2018-05-19 18:35:50 +0800

    ADD

---
+++ MvcProject/WebApplication/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using ModelsLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        [Route("Clothes")]
+        public ActionResult Index()
+        {
+            var service = new CategoriesService();
+            var query = service.ClassifyByCategoryName("上衣");
+            ViewData.Add("count", query.Count());
+            ViewData.Add("list", query);
+            return View();
+        }
+    }
+}
---
+++ MvcProject/WebApplication/Controllers/SignUpController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [RoutePrefix("Log")]
+    public class SignUpController : Controller
+    {
+        // GET: SignUp
+        [Route("Signup")]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
