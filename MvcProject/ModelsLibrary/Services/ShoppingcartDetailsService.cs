b704f95cd91291d7beb3e01cd69a8fd8e2a20095
A Kuan <s11996123@gmail.com>
2018-05-23 17:49:07 +0800

    service

--- MvcProject/ModelsLibrary/Services/ShoppingcartDetailsService.cs
+++ MvcProject/ModelsLibrary/Services/ShoppingcartDetailsService.cs
@@ -10,8 +10,6 @@ using System.Threading.Tasks;
 
 namespace ModelsLibrary.Services
 {
-    
-
     public class ShoppingcartDetailsService
     {
         private string sqlstr = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
