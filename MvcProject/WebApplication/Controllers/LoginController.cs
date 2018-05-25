3a2e1ab99531100c85bf57e116def7d283aa6412
BingBing <z42442412@gmail.com>
2018-05-25 12:10:47 +0800

    Update Login out Signup

--- MvcProject/WebApplication/Controllers/LoginController.cs
+++ MvcProject/WebApplication/Controllers/LoginController.cs
@@ -30,6 +30,18 @@ namespace WebApplication.Controllers
                 return View();
             }
 
+            var ticket = FormsAuthentication.Decrypt(cookie.Value);
+
+            if(ticket.UserData == "abcdefg")
+            {
+                ViewBag.IsAuthenticated = true;
+                ViewBag.User = ticket.Name;
+            }
+            else
+            {
+                ViewBag.IsAuthenticated = false;
+            }
+
             return View();
         }
 
@@ -38,7 +50,42 @@ namespace WebApplication.Controllers
         public ActionResult Login(loginModel model)
         {
             var service = new CustomerService();
-            return View();
+
+            var customer_list = service.GetAll().ToList();
+
+            if(customer_list.Any((x) => x.Account == model.User) == false)
+            {
+                return RedirectToAction("Login");
+            }
+
+            if(model.User == customer_list.Find((x) => x.Account == model.User).Account &&
+               model.Password == customer_list.Find((x) => x.Account == model.User).Password)
+            {
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, model.User, DateTime.Now, DateTime.Now.AddMinutes(30), false, "abcdefg");
+
+                var ticketData = FormsAuthentication.Encrypt(ticket);
+                var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, ticketData);
+                cookie.Expires = ticket.Expiration; //設定Cookie到期日與憑證同時
+
+                Response.Cookies.Add(cookie);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("loginModel", "Error");
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            var cookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            cookie.Expires = DateTime.Now;
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Login", "Login");
         }
     }
 }
