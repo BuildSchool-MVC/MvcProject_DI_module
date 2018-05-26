bbcdb63909b8e02ac824c311a001992c3cea9e7f
BingBing <z42442412@gmail.com>
2018-05-26 12:43:09 +0800

    測試新版面

--- MvcProject/WebApplication/Controllers/MainController.cs
+++ MvcProject/WebApplication/Controllers/MainController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using ModelsLibrary.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace WebApplication.Controllers
 {
     public class MainController : Controller
     {
         // GET: Main
-        public ActionResult Main()
+        public ActionResult Main_Layout()
         {
+            var cookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+
+            if (cookie == null)
+            {
+                ViewBag.Log = false;
+            }
+            else
+            {
+                ViewBag.Log = true;
+            }
+
             return View();
         }
     }
