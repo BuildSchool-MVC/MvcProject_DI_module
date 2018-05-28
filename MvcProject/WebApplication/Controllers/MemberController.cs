064ec2aa5284249d708ab43ca4bf2bacc7ae4563
A Kuan <s11996123@gmail.com>
2018-05-28 15:54:00 +0800

    merber

--- MvcProject/WebApplication/Controllers/MemberController.cs
+++ MvcProject/WebApplication/Controllers/MemberController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ModelsLibrary.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace WebApplication.Controllers
 {
@@ -12,7 +14,19 @@ namespace WebApplication.Controllers
         // GET: Member
         public ActionResult SearchMember()
         {
+            var service = new CustomerService();
+            var cookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            var ticket = FormsAuthentication.Decrypt(cookie.Value);
+            
+            var customer = service.FindByCustomerAccount(ticket.Name);
+            ViewBag.User = customer.Account;
+            ViewBag.Email = customer.Email;
+            ViewBag.Phone = customer.Phone;
+            ViewBag.Cash = customer.ShoppingCash;
+
+
             return View();
+            
         }
 
         public ActionResult UpdateMember()
