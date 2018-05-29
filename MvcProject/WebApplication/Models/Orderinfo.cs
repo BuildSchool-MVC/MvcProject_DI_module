82f4a5aaf5405174978aec7cd1d2ea85dfb1679f
BingBing <z42442412@gmail.com>
2018-05-29 22:02:15 +0800

    修改下定流程

---
+++ MvcProject/WebApplication/Models/Orderinfo.cs
@@ -0,0 +1,10 @@
+﻿using ModelsLibrary.DtO_Models;
+
+namespace WebApplication.Models
+{
+    public class Orderinfo
+    {
+        public Order Order { get; set; }
+        public string Address { get; set; }
+    }
+}
