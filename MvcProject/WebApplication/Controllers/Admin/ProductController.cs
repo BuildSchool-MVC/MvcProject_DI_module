33740fdec29d687a70e7a6330458de4abc351217
A Kuan <s11996123@gmail.com>
2018-05-31 21:41:09 +0800

    productupdate

--- MvcProject/WebApplication/Controllers/Admin/ProductController.cs
+++ MvcProject/WebApplication/Controllers/Admin/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ModelsLibrary.DtO_Models;
 using ModelsLibrary.Services;
 
 namespace WebApplication.Controllers.Admin
@@ -27,9 +28,29 @@ namespace WebApplication.Controllers.Admin
             return View();
         }
 
-        [Route("ProductUpdate")]
-        public ActionResult AdminProductUpdate()
+        [Route("ProductUpdate/{id}")]
+        public ActionResult AdminProductUpdate(int id)
         {
+            var productservice = new ProductsService();
+            var photoservice = new ProductPhotoService();
+            var product = productservice.FindByID(id);
+            var items = photoservice.FindById(id);
+            var result = new List<ProductPhoto>();
+
+            foreach(var item in items)
+            {
+                var photo = new ProductPhoto()
+                {
+                    PhotoID = item.PhotoID,
+                    PhotoPath = item.PhotoPath
+                };
+
+                result.Add(photo);
+            }
+
+            ViewData.Add("list", product);
+            ViewData.Add("photo", result);
+
             return View();
         }
     }
